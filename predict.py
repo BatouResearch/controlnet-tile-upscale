c87d248944fec055a6bcb973b72a43aa8c8da755
Claudia Len <claudia.len@estudiantat.upc.edu>
2024-07-05 11:09:44 +0000

    4096 and replicate model

--- predict.py
+++ predict.py
@@ -147,8 +147,25 @@ class Predictor(BasePredictor):
         mask_tile = Image.new("L", (tx2 - tx, ty2 - ty), 255)
         mask.paste(mask_tile, (abs(mx-tx), abs(ty-my)))
 
-        return mask, image.crop((mx, my, mx2, my2)), mx, my
+        return mask, image.crop((mx, my, mx2, my2)), mx, my, mx2, my2
 
+    def create_seam_masks(self, image_width, image_height, tile_width, tile_height, rows, cols, is_horizontal):
+        if is_horizontal:
+            mask = Image.new("L", (image_width, image_height), 0)
+            gradient = Image.linear_gradient("L").resize((tile_width, tile_height // 2))
+            for yi in range(rows - 1):
+                for xi in range(cols):
+                    mask.paste(gradient, (xi * tile_width, yi * tile_height + tile_height // 2))
+                    mask.paste(gradient.rotate(180), (xi * tile_width, (yi + 1) * tile_height))
+        else:
+            mask = Image.new("L", (image_width, image_height), 0)
+            gradient = Image.linear_gradient("L").rotate(90).resize((tile_width // 2, tile_height))
+            for xi in range(cols - 1):
+                for yi in range(rows):
+                    mask.paste(gradient, (xi * tile_width + tile_width // 2, yi * tile_height))
+                    mask.paste(gradient.rotate(180), ((xi + 1) * tile_width, yi * tile_height))
+        
+        return mask
 
     @torch.inference_mode()
     def predict(
@@ -175,7 +192,7 @@ class Predictor(BasePredictor):
         creativity: float = Input(
             description="Denoising strength. 1 means total destruction of the original image",
             default=0.35,
-            ge=0,
+            ge=0.1,
             le=1,
         ),
         hdr: float = Input(
@@ -212,17 +229,17 @@ class Predictor(BasePredictor):
         tile_size: int = Input(
             description="Size of partitions of the image. A 1/4 of the final resolution is recommended for optimal.",
             default=512,
-            choices=[128, 256, 374, 512, 768, 1024]
+            choices=[128, 256, 374, 512, 768, 1024, 1280]
         ),
         lora_sharpness_strength: float = Input(
             description="Strength of the image's sharpness. For it to be noticeable, it is recommended to use values between 2 and 5.",
-            default=1.0,
+            default=4.5,
             ge=-3.0,
             le=10.0,
         ),
         lora_details_strength: float = Input(
             description="Strength of the image's details",
-            default=1.0,
+            default=1.25,
             ge=-3.0,
             le=3.0,
         ),
@@ -232,13 +249,15 @@ class Predictor(BasePredictor):
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
+        self.pipe.unload_lora_weights()
+
         self.pipe.scheduler = SCHEDULERS[scheduler].from_config(self.pipe.scheduler.config)
         self.pipe.load_lora_weights("lora/add_sharpness.safetensors", adapter_name="sharp")
         self.pipe.load_lora_weights("lora/add_detail.safetensors", adapter_name="detail")
         self.pipe.load_lora_weights("lora/more_details.safetensors", adapter_name="more")
         self.pipe.set_adapters(["sharp","detail","more"], adapter_weights=[lora_sharpness_strength, lora_details_strength, lora_details_strength])
         self.pipe.enable_xformers_memory_efficient_attention()
-
+        
         generator = torch.Generator("cuda").manual_seed(seed)
         loaded_image = self.load_image(image)
         loaded_image = loaded_image.convert("RGB")
@@ -270,7 +289,7 @@ class Predictor(BasePredictor):
                     continue
                 tiles[row][col] = not tiles[row][col]
                 
-                mask, tile, mx, my = self.create_masks(final_image, tile_width, tile_height, row * tile_height, col * tile_width, pad)
+                mask, tile, mx, my, mx2, my2 = self.create_masks(final_image, tile_width, tile_height, row * tile_height, col * tile_width, pad)
     
                 args = {
                     "prompt": prompt,
@@ -289,21 +308,31 @@ class Predictor(BasePredictor):
                 outputs = self.pipe(**args)
                 processed_tile = outputs.images[0]
                 final_image = self.set_tile(final_image, mx, my, processed_tile)
-
-        args["strength"] = args["strength"] * 0.5
+        
         if resolution==4096:
+            args["strength"] = 0.15
+            h_mask = self.create_seam_masks(final_image.width, final_image.height, tile_width, tile_height, rows, cols, True)
+            v_mask = self.create_seam_masks(final_image.width, final_image.height, tile_width, tile_height, rows, cols, False)
+            full_mask = Image.composite(h_mask, v_mask, Image.new("L", final_image.size, 127))
             for row in range(0,2):
                 for col in range(0,2):
-                    mask, tile, mx, my = self.create_masks(final_image, 1024, math.ceil(1024 * (final_image.height/final_image.width)), row * 1024, col * 1024, 512)
+                    _, tile, mx, my, mx2, my2 = self.create_masks(final_image, 2048, math.ceil(2048 * (final_image.height/final_image.width)), row * 2048, col * 2048, 700)
+                    mask = full_mask.crop((mx, my, mx2, my2))
+                    
                     args["image"] = tile
                     args["control_image"] = tile
                     args["mask_image"] = mask
 
-                outputs = self.pipe(**args)
-                processed_tile = outputs.images[0]
-                final_image = self.set_tile(final_image, mx, my, processed_tile)  
+                    outputs = self.pipe(**args)
+                    processed_tile = outputs.images[0]
+                    new_image = self.set_tile(final_image, mx, my, processed_tile)
+                    final_image = Image.composite(final_image, new_image, full_mask) 
 
         else:
+            if creativity >= 0.5:
+                args["strength"] = 0.2
+            else:
+                args["strength"] = args["strength"] * 0.5
             args["image"] = final_image
             args["control_image"] = final_image
             args["mask_image"] = Image.new("L", final_image.size, 255)
@@ -311,7 +340,9 @@ class Predictor(BasePredictor):
             outputs = self.pipe(**args)
             final_image = outputs.images[0]
 
-        output_path = Path("/tmp/out-0.png")
-        final_image.save(output_path)
+        output_path = f"output.jpg"
+        final_image.save(output_path, "jpeg")
+
+
         
-        return output_path
+        return Path(output_path)
